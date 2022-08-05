258ccf1253dd1776785eff7b07d0d0d119b43e50
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-08-05 23:52:18 +0300

    Add even_number game

---
+++ mini_games/games/even_num.py
@@ -0,0 +1,10 @@
+import random
+
+
+def get_even_num_question():
+    RULES = 'Задача на определение четности числа'
+
+    num = random.randint(1, 1000)
+    question = f'Является ли {num} четным числом? (Да/Нет)'
+    answer = 'Да' if num % 2 == 0 else 'Нет'
+    return (question, answer, RULES)
