ab5081f39b371791140ad59cb73dc9284d9b26df
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-08-05 23:49:08 +0300

    Add calculator game

---
+++ mini_games/games/calculator.py
@@ -0,0 +1,23 @@
+import random
+from operator import add
+from operator import mul
+from operator import sub
+from operator import floordiv
+
+
+def get_calculator_question():
+    RULES = 'Задача на базовые математические операции с числами'
+    MATCH_SIGNS = (
+        ('+', add),
+        ('*', mul),
+        ('-', sub),
+        (':', floordiv),
+    )
+
+    sigh = random.choice(MATCH_SIGNS)
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+
+    question = f'{num1} {sigh[0]} {num2} = ?'
+    answer = sigh[1](num1, num2)
+    return (question, answer, RULES)
