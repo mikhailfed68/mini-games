e0f27dc6999c35b3a858de337e25ca188a859562
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-08-05 23:50:24 +0300

    Add test for even_number game

---
+++ tests/test_games/test_even_num.py
@@ -0,0 +1,9 @@
+from mini_games.games import even_num
+
+
+def test_get_even_num_question():
+    expected = even_num.get_even_num_question()
+    assert isinstance(expected, tuple)
+    assert isinstance(expected[0], str)
+    assert expected[1] == 'Да' or 'Нет'
+    assert expected[2] == 'Задача на определение четности числа'
